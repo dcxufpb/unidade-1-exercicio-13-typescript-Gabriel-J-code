24679bc58c84136acdb29f8d8d6b1db724e25e95
Gabriel de Jesus de <gabriel.franca@dce.ufpb.br>
2020-10-16 21:09:25 +0000

    Funcionando

--- venda.test.ts
+++ venda.test.ts
@@ -1,5 +1,62 @@
 import { Venda } from './venda';
+import { Loja } from './loja';
+import { Endereco } from './endereco';
 
 
-test('Venda ', () => {
+function rodarTestarRetorno(expected: String, venda: Venda): void  {
+
+	// action
+	let retorno = venda.dadosVenda();
+
+	// assertion		
+	expect(expected).toBe(retorno);
+}
+
+function verificarCampoObrigatorio(mensagemEsperada: String, venda: Venda):void{
+	try {
+		venda.dadosVenda();
+	} catch (e) {
+		expect(e.message).toBe(mensagemEsperada);
+	}
+}
+let NOME_LOJA: string = "Loja 1";
+let LOGRADOURO: string = "Log 1";
+let NUMERO:number = 10;
+let COMPLEMENTO: string = "C1";
+let BAIRRO: string = "Bai 1";
+let MUNICIPIO: string = "Mun 1";
+let ESTADO: string = "E1";
+let CEP: string = "11111-111";
+let TELEFONE: string = "(11) 1111-1111";
+let OBSERVACAO: string = "Obs 1";
+let CNPJ: string = "11.111.111/1111-11";
+let INSCRICAO_ESTADUAL: string = "123456789";
+
+let LOJA_COMPLETA:Loja = new Loja(NOME_LOJA,
+			new Endereco(LOGRADOURO, NUMERO, COMPLEMENTO, BAIRRO, MUNICIPIO, ESTADO, CEP), TELEFONE, OBSERVACAO,
+			CNPJ, INSCRICAO_ESTADUAL);
+
+//testes venda
+
+let datahora:Date = new Date(2015,10,29,11,9,47);
+let ccf: string = "021784";
+let coo: string = "035804";	
+
+//venda
+let TEXTO_ESPERADO_VENDA = "29/10/2015 11:09:47V CCF:021784 COO:035804";
+test('venda', () => {
+	rodarTestarRetorno(TEXTO_ESPERADO_VENDA,LOJA_COMPLETA.vender(datahora,ccf,coo));		
+});
+
+//venda sem ccf
+
+test('venda_valida_ccf', () => {
+	let VENDA_CCF_VAZIO: Venda  = LOJA_COMPLETA.vender(datahora,"",coo);
+	verificarCampoObrigatorio("O campo ccf da venda não é valido", VENDA_CCF_VAZIO);	
+});
+//venda sem coo
+
+test('venda_valida_coo', () => {
+	let VENDA_COO_VAZIO: Venda  = LOJA_COMPLETA.vender(datahora,ccf,"");
+	verificarCampoObrigatorio("O campo coo da venda não é valido", VENDA_COO_VAZIO);	
 });
--- venda.ts
+++ venda.ts
@@ -1,3 +1,43 @@
-export class Venda {
+import { Loja } from './loja';
 
+export class Venda {    
+
+    constructor(public loja :Loja, public DataHora :Date, public ccf :String, public coo :String) {}
+
+    public dadosVenda(): string {
+        this.validarCamposObrigatorios()
+
+        var aux = this.DataHora
+		var _date = Venda.intFormat(aux.getDate(),2)
+		var _month = Venda.intFormat(aux.getMonth(),2)
+		var _year = Venda.intFormat(aux.getFullYear(),2)
+		var _hours = Venda.intFormat(aux.getHours(),2)
+		var _minutes = Venda.intFormat(aux.getMinutes(),2)
+		var _seconds = Venda.intFormat(aux.getSeconds(),2)
+
+		var _datatime = `${_date}/${_month}/${_year} ${_hours}:${_minutes}:${_seconds}`		
+
+        var dados = `${_datatime}V CCF:${this.ccf} COO:${this.coo}`;       
+        return dados;
+    }
+
+	private static intFormat(num:number, tam:number): string {
+		var _num = num.toString()
+		while(_num.length<tam){
+			_num = "0" + _num
+		}
+		return _num		
+	}
+    
+    private validarCamposObrigatorios():void {		
+        if (!this.ccf){
+            throw new Error("O campo ccf da venda não é valido")
+        }
+        if (!this.coo){
+            throw new Error("O campo coo da venda não é valido")
+        }        
+    }
+    
 }
+
+
